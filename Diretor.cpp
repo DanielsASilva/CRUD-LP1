2e5c9bbddc6f2f1cca48520b1df29664ec42a802
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-12 17:07:06 -0300

    Update Diretor.cpp

--- Diretor.cpp
+++ Diretor.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "Diretor.h"
 #include <string>
     
 Diretor::Diretor() : Gerente(){
@@ -10,90 +10,90 @@ Diretor::~Diretor(){
 }
 
 int Diretor::getCodigo(){
-        
+    return codigo;  
 }
 
        
 std::string Diretor::getNome(){
-    
+    return nome;
 }
     
 std::string Diretor::getEndereco(){
-    
+    return endereco;
 }
 
 std::string Diretor::getTelefone(){
-    
+    return telefone;
 }
     
 int Diretor::getDataDia(){
-    
+    return dia;
 }
     
 int Diretor::getDataMes(){
-    
+    return mes;
 }
     
 int Diretor::getDataAno(){
-    
+    return ano;
 }
 
 int Diretor::getTipoFuncionario(){
-    
+    return tipoFuncionario;
 }
     
 float Diretor::getSalario(){
-    
+    return salario;
 }
 
 float Diretor::getAreaSupervisao(){
-    
+    return areaSupervisao;
 }
 
 std::string Diretor::getAreaFormacao(){
-  
+    return areaFormacao;
 }
 
 void Diretor::setCodigo(int code){
-    
+    codigo = code;
 }
 
 void Diretor::setNome(std::string name){
-    
+    nome = name;
 }
 
 void Diretor::setEndereco(float adress){
-    
+    endereco = adress;
 }
 
 void Diretor::setTelefone(int phone){
-    
+    telefone = phone;
 }
 
 void Diretor::setDataDia(int day){
-    
+    dia = day;
 }
 
 void Diretor::setDataMes(int month){
-    
+    mes = month;
 }
 
 void Diretor::setDataAno(int year){
-    
+    ano = year;
 }
 
 void Diretor::setTipoFuncionario(int functionaryType){
-    
+    tipoFuncionario = functionaryType;
 }
     
 void Diretor::setSalario(float salary){
-    
+    salario = salary;
 }
 
 void Diretor::setAreaSupervisao(std::string supervisionArea){
-    
+    areaSupervisao = supervisionArea;
 }
 
 void Diretor::setAreaFormacao(std::string academicWorkfield){
-  
+    areaFormacao = academicWorkField;
 }
