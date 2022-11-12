4c731b82fba27f0d3d3a75f4d0c4c2856dda9db2
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 21:42:31 -0300

    Update Operario.cpp

--- Operario.cpp
+++ Operario.cpp
@@ -1,1 +1,24 @@
+    
 
+    Operario();
+    ~Operario();
+
+    int getCodigo();
+    std::string getNome();
+    std::string getEndereco();
+    std::string getTelefone();
+    int getDataDia();
+    int getDataMes();
+    int getDataAno();
+    int getTipoFuncionario();
+    float getSalario();
+  
+    void setCodigo(int code);
+    void setNome(std::string name);
+    void setEndereco(float adress);
+    void setTelefone(int phone);
+    void setDataDia(int day);
+    void setDataMes(int month);
+    void setDataAno(int year);
+    void setTipoFuncionario(int functionaryType);
+    void setSalario(float salary);
