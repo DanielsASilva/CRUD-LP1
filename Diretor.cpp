7df8432b47b725dce9f4edce65c2a5e3a0c91249
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 22:23:53 -0300

    Diretor.cpp v1
    Diretor.cpp criado.

--- Diretor.cpp
+++ Diretor.cpp
@@ -1,1 +1,99 @@
+#include "Diretor.h"
+#include <string>
+    
+Diretor::Diretor() : Gerente(){
+        
+}
 
+Diretor::~Diretor(){
+    
+}
+
+int Diretor::getCodigo(){
+        
+}
+
+       
+std::string Diretor::getNome(){
+    
+}
+    
+std::string Diretor::getEndereco(){
+    
+}
+
+std::string Diretor::getTelefone(){
+    
+}
+    
+int Diretor::getDataDia(){
+    
+}
+    
+int Diretor::getDataMes(){
+    
+}
+    
+int Diretor::getDataAno(){
+    
+}
+
+int Diretor::getTipoFuncionario(){
+    
+}
+    
+float Diretor::getSalario(){
+    
+}
+
+float Diretor::getAreaSupervisao(){
+    
+}
+
+std::string Diretor::getAreaFormacao(){
+  
+}
+
+void Diretor::setCodigo(int code){
+    
+}
+
+void Diretor::setNome(std::string name){
+    
+}
+
+void Diretor::setEndereco(float adress){
+    
+}
+
+void Diretor::setTelefone(int phone){
+    
+}
+
+void Diretor::setDataDia(int day){
+    
+}
+
+void Diretor::setDataMes(int month){
+    
+}
+
+void Diretor::setDataAno(int year){
+    
+}
+
+void Diretor::setTipoFuncionario(int functionaryType){
+    
+}
+    
+void Diretor::setSalario(float salary){
+    
+}
+
+void Diretor::setAreaSupervisao(std::string supervisionArea){
+    
+}
+
+void Diretor::setAreaFormacao(std::string academicWorkfield){
+  
+}
