6816b6b9b015480e4a32327c265db2ea5461efcf
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 21:40:26 -0300

    Update Operario.h

--- Operario.h
+++ Operario.h
@@ -1,1 +1,43 @@
+#ifndef OPERARIO_H
+#define OPERARIO_H
+#include <string>
 
+class Operario : public Funcionario{
+  public:
+    Operario();
+    ~Operario();
+
+    int getCodigo() = 0;
+    std::string getNome() = 0;
+    std::string getEndereco() = 0;
+    std::string getTelefone();
+    int getDataDia();
+    int getDataMes();
+    int getDataAno();
+    int getTipoFuncionario();
+    float getSalario();
+  
+    void setCodigo(int code);
+    void setNome(std::string name);
+    void setEndereco(float adress);
+    void setTelefone(int phone);
+    void setDataDia(int day);
+    void setDataMes(int month);
+    void setDataAno(int year);
+    void setTipoFuncionario(int functionaryType);
+    void setSalario(float salary);
+  
+  protected:
+
+  private:
+    int codigo;
+    std::string nome;
+    std::string endereco;
+    std::string telefone;
+    int dataDia;
+    int dataMes;
+    int dataAno;
+    int tipoFuncionario;
+};
+
+#endif
