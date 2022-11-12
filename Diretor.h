4ada9603aff3493b6b0b75d40f7c3ec8cab6498b
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 22:14:32 -0300

    Diretor.h v1
    Diretor.h criado.

--- Diretor.h
+++ Diretor.h
@@ -1,1 +1,52 @@
+#ifndef DIREITO_H
+#define DIREITO_H
+#include <string>
+#include "Gerente.h"
 
+class Diretor : public Gerente{
+  public:
+    Diretor();
+    ~Direito();
+
+    int getCodigo();
+    std::string getNome();
+    std::string getEndereco();
+    std::string getTelefone();
+    int getDataDia();
+    int getDataMes();
+    int getDataAno();
+    int getTipoFuncionario();
+    float getSalario();
+    std::string getAreaSupervisao();
+    setd::string getAreaFormacao();
+  
+    void setCodigo(int code);
+    void setNome(std::string name);
+    void setEndereco(float adress);
+    void setTelefone(int phone);
+    void setDataDia(int day);
+    void setDataMes(int month);
+    void setDataAno(int year);
+    void setTipoFuncionario(int functionaryType);
+    void setSalario(float salary);
+    void setAreaSupervisao(std::string supervisionArea);
+    void setAreaFormacao(std::string academicWorkfield);
+  
+  protected:
+
+  private:
+    int codigo;
+    std::string nome;
+    std::string endereco;
+    std::string telefone;
+    int dataDia;
+    int dataMes;
+    int dataAno;
+    int tipoFuncionario;
+  
+    std::string areaSupervisao;
+    
+    std::string areaFormacao;
+};
+
+#endif
