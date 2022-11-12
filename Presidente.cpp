1e6c72596f6180616d2e6f9e8bfe437385080f23
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 22:43:40 -0300

    Presidente.cpp v1
    Presidente.cpp criado.

--- Presidente.cpp
+++ Presidente.cpp
@@ -1,1 +1,103 @@
+#include "Presidente.h"
+#include <string>
+    
+Presidente::Presidente() : Funcionario(){
+        
+}
 
+Presidente::~Presidente(){
+    
+}
+
+int Presidente::getCodigo(){
+        
+}
+
+       
+std::string Presidente::getNome(){
+    
+}
+    
+std::string Presidente::getEndereco(){
+    
+}
+
+std::string Presidente::getTelefone(){
+    
+}
+    
+int Presidente::getDataDia(){
+    
+}
+    
+int Presidente::getDataMes(){
+    
+}
+    
+int Presidente::getDataAno(){
+    
+}
+
+int Presidente::getTipoFuncionario(){
+    
+}
+    
+float Presidente::getSalario(){
+    
+}
+
+float Presidente::getAreaSupervisao(){
+    
+}
+
+std::string Presidente::getAreaFormacao(){
+    
+}
+
+std::string setAreaFormacao(std::string academicWorkfield){
+  
+}
+
+void Presidente::setCodigo(int code){
+    
+}
+
+void Presidente::setNome(std::string name){
+    
+}
+
+void Presidente::setEndereco(float adress){
+    
+}
+
+void Presidente::setTelefone(int phone){
+    
+}
+
+void Presidente::setDataDia(int day){
+    
+}
+
+void Presidente::setDataMes(int month){
+    
+}
+
+void Presidente::setDataAno(int year){
+    
+}
+
+void Presidente::setTipoFuncionario(int functionaryType){
+    
+}
+    
+void Presidente::setSalario(float salary){
+    
+}
+
+void setAreaFormacao(std::string academicWorkfield){
+  
+}
+
+void Presidente::setAreaFormacao(std::string academicDegree){
+    
+}
