fe40888a1e0e1cdb2460ad8ce1314221524f1baa
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 22:04:23 -0300

    Gerente.cpp v1
    Gerente.cpp criado

--- Gerente.cpp
+++ Gerente.cpp
@@ -1,1 +1,83 @@
+#include "Gerente.h"
 
+    
+Gerente::Gerente() : Funcionario(){
+        
+}
+
+Gerente::~Gerente(){
+    
+}
+
+int Gerente::getCodigo(){
+        
+}
+
+       
+std::string Gerente::getNome(){
+    
+}
+    
+std::string Gerente::getEndereco(){
+    
+}
+
+std::string Gerente::getTelefone(){
+    
+}
+    
+int Gerente::getDataDia(){
+    
+}
+    
+int Gerente::getDataMes(){
+    
+}
+    
+int Gerente::getDataAno(){
+    
+}
+
+int Gerente::getTipoFuncionario(){
+    
+}
+    
+float Gerente::getSalario(){
+    
+}
+  
+void Gerente::setCodigo(int code){
+    
+}
+
+void Gerente::setNome(std::string name){
+    
+}
+
+void Gerente::setEndereco(float adress){
+    
+}
+
+void Gerente::setTelefone(int phone){
+    
+}
+
+void Gerente::setDataDia(int day){
+    
+}
+
+void Gerente::setDataMes(int month){
+    
+}
+
+void Gerente::setDataAno(int year){
+    
+}
+
+void Gerente::setTipoFuncionario(int functionaryType){
+    
+}
+    
+void Gerente::setSalario(float salary){
+    
+}
