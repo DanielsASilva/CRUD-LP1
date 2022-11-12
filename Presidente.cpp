3f42066126336f1a3adc710d466a906326b28c37
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-12 17:17:14 -0300

    Update Presidente.cpp

--- Presidente.cpp
+++ Presidente.cpp
@@ -10,94 +10,90 @@ Presidente::~Presidente(){
 }
 
 int Presidente::getCodigo(){
-        
+    return codigo;
 }
 
        
 std::string Presidente::getNome(){
-    
+    return nome;
 }
     
 std::string Presidente::getEndereco(){
-    
+    return endereco;
 }
 
 std::string Presidente::getTelefone(){
-    
+    return telefone;
 }
     
 int Presidente::getDataDia(){
-    
+    return dia;
 }
     
 int Presidente::getDataMes(){
-    
+    return mes;
 }
     
 int Presidente::getDataAno(){
-    
+    return ano;
 }
 
 int Presidente::getTipoFuncionario(){
-    
+    return tipoFuncionario;
 }
     
 float Presidente::getSalario(){
-    
-}
-
-std::string Presidente::getAreaSupervisao(){
-    
+    return salario;
 }
 
 std::string Presidente::getAreaFormacao(){
-    
+    return areaFormacao;
 }
 
-std::string setAreaFormacao(std::string academicWorkfield){
-  
+std::string Presidente::getAreaAcademicaMax(){
+    return areaAcademicaMax;
 }
 
 void Presidente::setCodigo(int code){
-    
+    codigo = code;
 }
 
 void Presidente::setNome(std::string name){
-    
+    nome = name;
 }
 
 void Presidente::setEndereco(float adress){
-    
+    endereco = adress;
 }
 
 void Presidente::setTelefone(int phone){
-    
+    telefone = phone;
 }
 
 void Presidente::setDataDia(int day){
-    
+    dia = day;
 }
 
 void Presidente::setDataMes(int month){
-    
+    mes = month;
 }
 
 void Presidente::setDataAno(int year){
-    
+    ano = year;
 }
 
 void Presidente::setTipoFuncionario(int functionaryType){
-    
+    tipoFuncionario = functionaryType;
 }
     
 void Presidente::setSalario(float salary){
-    
+    salario = salary;
 }
 
-void setAreaFormacao(std::string academicDegree){
-  
+void Presidente::setAreaFormacao(std::string academicWorkfield){
+    areaFormacao = academicWorkfield;
 }
 
-void Presidente::setAreaFormacao(std::string academicDegree){
-    
+void Presidente::setAreaAcademicaMax(std::string academicDegree){
+    areaAcademicaMax = academicDegree;
 }
