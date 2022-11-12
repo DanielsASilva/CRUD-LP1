c63b5c0742a7d5990e1baa176250b8c04df2bcf0
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-11 21:36:08 -0300

    Funcionario.h supostamente 100%
    todos gets, sets e variáveis declaradas.

--- Funcionario.h
+++ Funcionario.h
@@ -1,1 +1,44 @@
+#ifndef FUNCIONARIO_H
+#define FUNCIONARIO_H
+#include <string>
 
+class Funcionario{
+  public:
+    Funcionario();
+    ~Funcionario();
+
+    virtual int getCodigo() = 0;
+    virtual std::string getNome() = 0;
+    virtual std::string getEndereco() = 0;
+    virtual std::string getTelefone();
+    virtual int getDataDia() = 0;
+    virtual int getDataMes() = 0;
+    virtual int getDataAno() = 0;
+    virtual int getTipoFuncionario() = 0;
+    virtual float getSalario() = 0;
+  
+    virtual void setCodigo(int code) = 0;
+    virtual void setNome(std::string name) = 0;
+    virtual void setEndereco(float adress) = 0;
+    virtual void setTelefone(int phone) = 0;
+    virtual void setDataDia(int day) = 0;
+    virtual void setDataMes(int month) = 0;
+    virtual void setDataAno(int year) = 0;
+    virtual void setTipoFuncionario(int functionaryType) = 0;
+    virtual void setSalario(float salary) = 0;
+  
+  protected:
+    int codigo;
+    std::string nome;
+    std::string endereco;
+    std::string telefone;
+    int dataDia;
+    int dataMes;
+    int dataAno;
+    int tipoFuncionario;
+    float salario;
+  private:
+
+};
+
+#endif
