580d65b84c0093636a9da3f282e2674853070209
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-14 16:31:33 -0300

    Update Gerenciador.cpp

--- Gerenciador.cpp
+++ Gerenciador.cpp
@@ -1,1 +1,37 @@
 #include "Gerenciador.h"
+#include <string>
+#include <iostream>
+
+Gerenciador::Gerenciador(){
+  
+}
+
+Gerenciador::~Gerenciador(){
+  
+}
+
+int Gerenciador::adicionarFuncionario(){
+  
+}
+
+
+int Gerenciador::editarFuncionario(){
+  
+}
+  
+int Gerenciador::excluirFuncionario(){
+  
+}
+
+int Gerenciador::exibirFuncionario(){
+  
+}
+
+
+int Gerenciador::exibirLista(){
+  
+}
+
+int Gerenciador::exibirListaTipo(){
+  
+}
