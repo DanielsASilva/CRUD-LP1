9f34b65c7bac9535b19bdf66713c2f6d2546b32a
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-14 16:44:56 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,41 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World!!" << endl;
+  int opcao;
+  
+  Gerenciador gerenciador;
+  
+  cout << "SISTEMA DE GERENCIAMENTO DE FUNCIONÁRIOS" << endl;
+  cout << "1. Adicionar Funcionário\n" <<
+  << "2. Editar Funcionário\n"
+  << "3. Excluir Funcionário\n"
+  << "4. Exibir Funcionário\n"
+  << "5. Exibir Lista\n"
+  << "6. Exibir Lista por Tipo\n";
+  
+  cin >> opcao;
+  cin.ignore();
+  
+  switch(opcao){
+    case 1:
+      gerenciador.adicionarFuncionario();
+      break;
+    case 2:
+      gerenciador.editarFuncionario();
+      break;
+    case 3:
+      gerenciador.excluirFuncionario();
+      break;
+    case 4:
+      gerenciador.exibirFuncionario();
+      break;
+    case 5:
+      gerenciador.exibirLista();
+      break;
+    case 6;
+      gerenciador.exibirListaTipo();
+      break;
+  }
+  
   return 0;
 }
