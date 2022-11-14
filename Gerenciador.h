db436c90be2be6f074d714d5df0b25e59252e8a0
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-14 16:22:16 -0300

    Update Gerenciador.h

--- Gerenciador.h
+++ Gerenciador.h
@@ -1,7 +1,26 @@
 #ifend GERENCIADOR_H
 #define GERENCIADOR_H
+#include "Funcionario.h"
+#include "Operador.h"
+#include "Gerente.h"
+#include "Diretor.h"
+#include "Presidente.h"
 
 class Gerenciador{
+  public:
+  Gerenciador();
+  ~Gerenciador();
+  
+  int adicionarFuncionario();
+  int editarFuncionario();
+  int excluirFuncionario();
+  int exibirFuncionario();
+  int exibirLista();
+  int exibirListaTipo();
+  
+  protected:
+  
+  private:
   
 }
 
