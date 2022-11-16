535d35a125471c6bdc83671cc3e457c802776afa
DanielsASilva <103001252+DanielsASilva@users.noreply.github.com>
2022-11-16 13:29:56 -0300

    Update Gerenciador.cpp

--- Gerenciador.cpp
+++ Gerenciador.cpp
@@ -63,12 +63,12 @@ int Gerenciador::adicionarFuncionario(){
   //recebem a área de supervisão, os dois últimos recebem área de formação e "Presidente" recebe a formação academica
     switch(tipo){
         case 1:
-        vectorFuncionarios.push_back(new Operador(codigoFuncionario, nome, endereco, dia, mes, ano, "Operador", telefone, salario));
+        vectorFuncionarios.push_back(new Operador(codigoFuncionario, nome, endereco, dia, mes, ano, telefone, "Operador", salario));
         break;
         case 2:
         cout << "Digite a área de supervisão do gerente: ";
         getline(cin, areaSupervisao);
-        
+
         vectorFuncionarios.push_back(new Gerente(codigoFuncionario, nome, endereco, dia, mes, ano, "Gerente", telefone, salario, areaSupervisao));
         break;
         case 3:
@@ -78,7 +78,7 @@ int Gerenciador::adicionarFuncionario(){
         cout << "Digite a área de formação do diretor: ";
         getline(cin, areaFormacao);
         
-        vectorFuncionarios.push_back(new Gerente(codigoFuncionario, nome, endereco, dia, mes, ano, "Diretor", telefone,  salario, areaSupervisao, areaFormacao));
+        vectorFuncionarios.push_back(new Diretor(codigoFuncionario, nome, endereco, dia, mes, ano, "Diretor", telefone, salario, areaSupervisao, areaFormacao));
         break;
         case 4:
         cout << "Digite a área de formação do presidente: ";
@@ -86,15 +86,15 @@ int Gerenciador::adicionarFuncionario(){
         
         cout << "Digite a formação academica máxima do presidente: ";
         getline(cin, areaAcademicaMax);
-        
-        vectorFuncionarios.push_back(new Presidente(codigoFuncionario, nome, endereco, dia, mes, ano, "Presidente", telefone, salario, areaFormacao, areaAcademicaMax));
+
+        vectorFuncionarios.push_back(new Presidente(codigoFuncionario, nome, endereco, dia, mes, ano, telefone, "Presidente", salario, areaFormacao, areaAcademicaMax));
         break;
         default:
         cout << "Opção inválida, tente novamente!\n";
         break;
     }
     
-    fstream fs;
+    fstream stream;
     
   // Arquivo é aberto e as informações gerais previamente cadastradas são adicionadas
     stream.open("./listaFuncionario.txt", fstream::out);
@@ -103,30 +103,30 @@ int Gerenciador::adicionarFuncionario(){
     }
   
     for(int c = 0; c < vectorFuncionarios.size(); c++){
-      stream << listaFuncionario.at(c)->getCodigo() << endl;
-      stream << listaFuncionario.at(c)->getNome() << endl;
-      stream << listaFuncionario.at(c)->getEndereco() << endl;
-      stream << listaFuncionario.at(c)->getDia() << endl;
-      stream << listaFuncionario.at(c)->getMes() << endl;
-      stream << listaFuncionario.at(c)->getAno() << endl;
-      stream << listaFuncionario.at(c)->getPosicao() << endl;
-      stream << listaFuncionario.at(c)->getTelefone() << endl;
-      stream << listaFuncionario.at(c)->getSalario() << endl;
+      stream << vectorFuncionarios.at(c)->getCodigo() << endl;
+      stream << vectorFuncionarios.at(c)->getNome() << endl;
+      stream << vectorFuncionarios.at(c)->getEndereco() << endl;
+      stream << vectorFuncionarios.at(c)->getDataDia() << endl;
+      stream << vectorFuncionarios.at(c)->getDataMes() << endl;
+      stream << vectorFuncionarios.at(c)->getDataAno() << endl;
+      stream << vectorFuncionarios.at(c)->getPosicao() << endl;
+      stream << vectorFuncionarios.at(c)->getTelefone() << endl;
+      stream << vectorFuncionarios.at(c)->getSalario() << endl;
       
       // Em seguida, as informações específicas por cargo são adicionadas
-      switch(listaFuncionarios.at(c)->getTipo()){
+      switch(vectorFuncionarios.at(c)->getTipoFuncionario()){
         case 1:
           break;
         case 2:
-          stream << listaFuncionario.at(c)->getAreaSupervisao() << endl;
+          stream << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
           break;
         case 3:
-          stream << listaFuncionario.at(c)->getAreaSupervisao() << endl;
-          stream << listaFuncionario.at(c)->getAreaFormacao() << endl;
+          stream << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+          stream << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
           break;
         case 4:
-          stream << listaFuncionario.at(c)->getAreaFormacao() << endl;
-          stream << listaFuncionario.at(c)->getAreaAcademicaMax() << endl;
+          stream << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+          stream << vectorFuncionarios.at(c)->getAreaAcademicaMax() << endl;
           break;
       }
       
@@ -136,27 +136,28 @@ int Gerenciador::adicionarFuncionario(){
 
 
 int Gerenciador::editarFuncionario(){
-  int codigo, indice, check, opt;
+  int codigo, indice, check, opt, dia, mes, ano;
+  string posicao, endereco;
+  float salario;
   cout << "Existem " << vectorFuncionarios.size() << " funcionários na empresa, digite o código do funcionário para edição: ";
   cin >> codigo;
   cin.ignore();
   
   // Com o código respectivo ao funcionário em questão, as informações que o usuário pretende editar são reescritas e atualizadas 
   while(1){
-    for(int c = 0; c < vectorFuncionarios.size; c++){
-       if(listaFuncionarios.at(c)->getCodigo() == codigo){
+    for(int c = 0; c < vectorFuncionarios.size(); c++){
+       if(vectorFuncionarios.at(c)->getCodigo() == codigo){
           indice = c;
           check++;
        }
     }
     if(check == 0){
-      cout << "código iválido << endl; 
+      cout << "código inválido" << endl; 
     }else{
       break; 
     }  
   }
   
-  //
   cout << "Editando o funcionário " << codigo << ".\n"
   << "1. Código: " << endl
   << "2. Dia de ingresso: " << endl
@@ -180,100 +181,191 @@ int Gerenciador::editarFuncionario(){
       cout << "\nEdição Concluída\n";
       break;
     case 2:
-      int dia;
       cout << "Insira o novo dia de ingresso: ";
       cin >> dia;
       cin.ignore();
       
-      vectorFuncionarios.at(indice)->setDia(dia);
+      vectorFuncionarios.at(indice)->setDataDia(dia);
       cout << "\nEdição Concluída\n";
       break;
     case 3:
-      int mes;
       cout << "Insira o novo mês de ingresso: ";
       cin >> mes;
       cin.ignore();
       
-      vectorFuncionarios.at(indice)->setMes(mes);
+      vectorFuncionarios.at(indice)->setDataMes(mes);
       cout << "\nEdição Concluída\n";
       break;
     case 4:
-      int ano;
-      cout << "Insira o novo ano de ingresso: "
+      cout << "Insira o novo ano de ingresso: ";
       cin >> ano;
       cin.ignore();
         
-      vectorFuncionarios.at(indice)->setAno(ano);
+      vectorFuncionarios.at(indice)->setDataAno(ano);
       cout << "\nEdição Concluída\n";
       break;
     case 5:
-      string endereco;
       cout << "Insira o novo endereço: ";
       getline(cin, endereco);
       
-      vectorFuncionarios.at(indice)->setEndereco();
+      vectorFuncionarios.at(indice)->setEndereco(endereco);
       cout << "\nEdição Concluída\n";
       break;
-    case 6:
-      string posicao;
+    case 6:{
       cout << "Insira a nova posição: ";
       getline(cin, posicao);
       
       vectorFuncionarios.at(indice)->setPosicao(posicao);
       cout << "\nEdição Concluída\n";
-      break;
-    case 7:
-      float salario;
+      }break;
+    case 7:{
       cout << "Insira o novo salário: ";
       cin >> salario;
-      cin.ignore;
+      cin.ignore();
       
       vectorFuncionarios.at(indice)->setSalario(salario);
       cout << "\nEdição Concluída\n";
-      break;
+      }break;
   }
+  
+  }
+  
 }
   
 int Gerenciador::excluirFuncionario(){
-  
+  int codigo, indice, check;
+  string opt;
+  cout << "Existem " << vectorFuncionarios.size() << " funcionários na empresa, digite o código do funcionário para exclusão: ";
+  cin >> codigo;
+
+  for(int c = 0; c < vectorFuncionarios.size(); c++){
+    if(vectorFuncionarios.at(c)->getCodigo() == codigo){
+      indice = c;
+      check++;
+    }if(check == 0){
+      cout << "código inválido" << endl;
+    }else{
+      break;
+    }
+  }
+}
+
+for(int c = 0; c < vectorFuncionarios.size(); c++){
+  if(vectorFuncionarios.at(c)->getCodigo() == codigo){
+    cout << "Você deseja excluir o funcionário " << vectorFuncionarios.at(c)->getCodigo() << " - " << vectorFuncionarios.at(c)->getNome() << "? (y/n)";
+    getline(cin, opt);
+  }
+  if(opt == "y"){
+    vectorFuncionarios.erase(vectorFuncionarios.begin() + c);
+    cout << "Funcionário deletado com sucesso!" << endl
+  }else{
+    cout << "Funcionário não foi deletado!" << endl;
+  }
 }
 
 // Um funcionário específico terá seus dados gerais listados em ordem
 int Gerenciador::exibirFuncionario(){
+  int codigo;
+  cout << "Digite o código do funcionário a ser exibido: ";
+  cin >> codigo;
+
   for(int c = 0; c < vectorFuncionarios.size(); c++){
-    cout << "\nCódigo do funcionário: "<< listaFuncionarios.at(c)->getCodigo() << endl
-    << "Nome: " << listaFuncionarios.at(c)->getNome() << endl
-    << "Endereco: " << listaFuncionarios.at(c)->getEndereco() << endl
-    << "Telefone: " << listaFuncionarios.at(c)->getTelefone() << endl
-    << "Data de ingresso: " << listaFuncionarios.at(c)->getDia() << "/" << listaFuncionarios.at(c)->getMes() << "/" listaFuncionarios.at(c)->getAno() << endl
-    << "Posição na empresa: " << listaFuncionarios.at(c)->getPosicao() << endl
-    << "Salario: " << listaFuncionarios.at(c)->getSalario() << endl;
+    if(vectorFuncionarios.at(c)->getCodigo() == codigo){
+      cout << "\nCódigo do funcionário: "<< vectorFuncionarios.at(c)->getCodigo() << endl
+      << "Nome: " << vectorFuncionarios.at(c)->getNome() << endl
+      << "Endereco: " << vectorFuncionarios.at(c)->getEndereco() << endl
+      << "Telefone: " << vectorFuncionarios.at(c)->getTelefone() << endl
+      << "Data de ingresso: " << vectorFuncionarios.at(c)->getDataDia() << "/" <<     vectorFuncionarios.at(c)->getDataMes() << "/" << vectorFuncionarios.at(c)->getDataAno() << endl
+      << "Posição na empresa: " << vectorFuncionarios.at(c)->getPosicao() << endl
+      << "Salario: " << vectorFuncionarios.at(c)->getSalario() << endl;
     
     // Caso o funcionário em questão não seja "Operador", seus dados específicos também serão exibidos
-    switch(listaFuncionarios.at(c)->getTipo()){
+      switch(vectorFuncionarios.at(c)->getTipoFuncionario()){
+        case 1:
+          break;
+        case 2:
+          cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+          break;
+        case 3:
+          cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+          cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+          break;
+        case 4:
+          cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+          cout << "Área de formação acadêmica máxima: " << vectorFuncionarios.at(c)->getAreaAcademicaMax() << endl;
+          break;
+      }
+  }
+    
+}
+}
+
+int Gerenciador::exibirLista(){
+  for(int c = 0; c < vectorFuncionarios.size(); c++){
+    cout << "\nCódigo do funcionário: "<< vectorFuncionarios.at(c)->getCodigo() << endl
+    << "Nome: " << vectorFuncionarios.at(c)->getNome() << endl
+    << "Endereco: " << vectorFuncionarios.at(c)->getEndereco() << endl
+    << "Telefone: " << vectorFuncionarios.at(c)->getTelefone() << endl
+    << "Data de ingresso: " << vectorFuncionarios.at(c)->getDataDia() << "/" << vectorFuncionarios.at(c)->getDataMes() << "/" << vectorFuncionarios.at(c)->getDataAno() << endl
+    << "Posição na empresa: " << vectorFuncionarios.at(c)->getPosicao() << endl
+    << "Salario: " << vectorFuncionarios.at(c)->getSalario() << endl;
+    
+    // Caso o funcionário em questão não seja "Operador", seus dados específicos também serão exibidos
+    switch(vectorFuncionarios.at(c)->getTipoFuncionario()){
       case 1:
         break;
       case 2:
-        cout << "Área de supervisão: " << listaFuncionarios.at(c)->getAreaSupervisao() << endl;
+        cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
         break;
       case 3:
-        cout << "Área de supervisão: " << listaFuncionarios.at(c)->getAreaSupervisao() << endl;
-        cout << "Área de formação: " << listaFuncionarios.at(c)->getAreaFormacao() << endl;
+        cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+        cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
         break;
       case 4:
-        cout << "Área de formação: " << listaFuncionarios.at(c)->getAreaFormacao() << endl;
-        cout << "Área de formação acadêmica máxima: " << listaFuncionarios.at(c)->getAreaAcademicaMax() << endl;
+        cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+        cout << "Área de formação acadêmica máxima: " << vectorFuncionarios.at(c)->getAreaAcademicaMax() << endl;
         break;
     }
   }
   
 }
 
-
-int Gerenciador::exibirLista(){
-  
-}
-
 int Gerenciador::exibirListaTipo(){
+  int tipo;
+  cout << "Insira o tipo de funcionário que gostaria de exibir\n"
+    << "1. Operador\n"
+    << "2. Gerente\n"
+    << "3. Diretor\n"
+    << "4. Presidente\n";
+  cin >> tipo;
   
+  for(int c = 0; c < vectorFuncionarios.size(); c++){
+    if(vectorFuncionarios.at(c)->getTipo() == tipo){
+      cout << "\nCódigo do funcionário: "<< vectorFuncionarios.at(c)->getCodigo() << endl
+      << "Nome: " << vectorFuncionarios.at(c)->getNome() << endl
+      << "Endereco: " << vectorFuncionarios.at(c)->getEndereco() << endl
+      << "Telefone: " << vectorFuncionarios.at(c)->getTelefone() << endl
+      << "Data de ingresso: " << vectorFuncionarios.at(c)->getDataDia() << "/" <<     vectorFuncionarios.at(c)->getDataMes() << "/" << vectorFuncionarios.at(c)->getDataAno() << endl
+      << "Posição na empresa: " << vectorFuncionarios.at(c)->getPosicao() << endl
+      << "Salario: " << vectorFuncionarios.at(c)->getSalario() << endl;
+    
+    // Caso o funcionário em questão não seja "Operador", seus dados específicos também serão exibidos
+      switch(vectorFuncionarios.at(c)->getTipoFuncionario()){
+        case 1:
+          break;
+        case 2:
+          cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+          break;
+        case 3:
+          cout << "Área de supervisão: " << vectorFuncionarios.at(c)->getAreaSupervisao() << endl;
+          cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+          break;
+        case 4:
+          cout << "Área de formação: " << vectorFuncionarios.at(c)->getAreaFormacao() << endl;
+          cout << "Área de formação acadêmica máxima: " << vectorFuncionarios.at(c)->getAreaAcademicaMax() << endl;
+          break;
+      }
+  }
+    
+}
 }
