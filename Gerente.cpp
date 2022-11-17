1cd0150d0bb824033a2b36bb5b0f154b4fca755d
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-17 12:23:08 -0300

    Update Gerente.cpp

--- Gerente.cpp
+++ Gerente.cpp
@@ -5,6 +5,19 @@ Gerente::Gerente() : Funcionario(){
         
 }
 
+Gerente::Gerente(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string posicao, std::string telefone, float salario, std::string areaSupervisao){
+    this->codigo = codigoFuncionario;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->dataDia = dia;
+    this->dataMes = mes;
+    this->dataAno = ano;
+    this->posicao = posicao;
+    this->telefone = telefone;
+    this->salario = salario;
+    this->areaSupervisao = areaSupervisao;
+ }
+
 Gerente::~Gerente(){
     
 }
@@ -27,23 +40,27 @@ std::string Gerente::getTelefone(){
 }
     
 int Gerente::getDataDia(){
-    return dia;
+    return dataDia;
 }
     
 int Gerente::getDataMes(){
-    return mes;
+    return dataMes;
 }
     
 int Gerente::getDataAno(){
-    return ano;
+    return dataAno;
 }
 
 int Gerente::getTipoFuncionario(){
     return tipoFuncionario;
 }
-    
+
+std::string Gerente::getPosicao(){
+    return posicao;   
+}
+
 float Gerente::getSalario(){
-    return tipoSalario;
+    return salario;
 }
 
 std::string Gerente::getAreaSupervisao(){
@@ -58,30 +75,34 @@ void Gerente::setNome(std::string name){
     nome = name;
 }
 
-void Gerente::setEndereco(float adress){
+void Gerente::setEndereco(std::string adress){
     endereco = adress;
 }
 
 void Gerente::setTelefone(int phone){
-    telefone = fone;
+    telefone = phone;
 }
 
 void Gerente::setDataDia(int day){
-    dia = day;
+    dataDia = day;
 }
 
 void Gerente::setDataMes(int month){
-    mes = month;
+    dataMes = month;
 }
 
 void Gerente::setDataAno(int year){
-    ano = year;
+    dataAno = year;
 }
 
 void Gerente::setTipoFuncionario(int functionaryType){
     tipoFuncionario = functionaryType;
 }
-    
+
+void Gerente::setPosicao(std::string position){
+    posicao = position;   
+}
+
 void Gerente::setSalario(float salary){
     salario = salary;
 }
