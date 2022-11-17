bf90b82c861ef26368904b726de4af297b2e2750
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-17 12:21:24 -0300

    Update Diretor.h

--- Diretor.h
+++ Diretor.h
@@ -1,12 +1,14 @@
-#ifndef DIREITO_H
-#define DIREITO_H
+#ifndef DIRETOR_H
+#define DIRETOR_H
 #include <string>
 #include "Gerente.h"
 
-class Diretor : public Gerente{
+//.h padrão com "get"s e "set"s. Na classe diretor, os dados Area de Supervisao e Area de Formacao estão inclusos
+class Diretor : public Funcionario{
   public:
     Diretor();
-    ~Direito();
+    Diretor(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string posicao, std::string telefone, float salario, std::string areaSupervisao, std::string areaFormacao);
+    ~Diretor();
 
     int getCodigo();
     std::string getNome();
@@ -16,22 +18,27 @@ class Diretor : public Gerente{
     int getDataMes();
     int getDataAno();
     int getTipoFuncionario();
+    std::string getPosicao();
     float getSalario();
     std::string getAreaSupervisao();
     std::string getAreaFormacao();
+    //std::string getAreaAcademicaMax();
   
     void setCodigo(int code);
     void setNome(std::string name);
-    void setEndereco(float adress);
+    void setEndereco(std::string adress);
     void setTelefone(int phone);
     void setDataDia(int day);
     void setDataMes(int month);
     void setDataAno(int year);
     void setTipoFuncionario(int functionaryType);
+    void setPosicao(std::string position);
     void setSalario(float salary);
     void setAreaSupervisao(std::string supervisionArea);
     void setAreaFormacao(std::string academicWorkfield);
   
+    //void setAreaAcademicaMax(std::string academicDegree); 
+
   protected:
 
   private:
@@ -39,6 +46,7 @@ class Diretor : public Gerente{
     std::string nome;
     std::string endereco;
     std::string telefone;
+    std::string posicao;
     int dataDia;
     int dataMes;
     int dataAno;
