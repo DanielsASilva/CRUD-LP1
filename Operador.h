e6e51a1554ea8e3b5b33c3f443d39f9ede6e90cd
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-17 12:24:09 -0300

    Update Operador.h

--- Operador.h
+++ Operador.h
@@ -6,6 +6,7 @@
 class Operador : public Funcionario{
   public:
     Operador();
+    Operador(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario);
     ~Operador();
 
     int getCodigo();
@@ -16,17 +17,25 @@ class Operador : public Funcionario{
     int getDataMes();
     int getDataAno();
     int getTipoFuncionario();
+    std::string getPosicao();
     float getSalario();
+    //std::string getAreaSupervisao();
+    //std::string getAreaFormacao();
+    //std::string getAreaAcademicaMax();
   
     void setCodigo(int code);
     void setNome(std::string name);
-    void setEndereco(float adress);
+    void setEndereco(std::string adress);
     void setTelefone(int phone);
     void setDataDia(int day);
     void setDataMes(int month);
     void setDataAno(int year);
+    void setPosicao(std::string position);
     void setTipoFuncionario(int functionaryType);
     void setSalario(float salary);
+    //void setAreaSupervisao(std::string supervisionArea);
+    //void setAreaFormacao(std::string academicWorkfield);
+    //void setAreaAcademicaMax(std::string academicDegree); 
   
   protected:
 
@@ -35,6 +44,7 @@ class Operador : public Funcionario{
     std::string nome;
     std::string endereco;
     std::string telefone;
+    std::string posicao;
     int dataDia;
     int dataMes;
     int dataAno;
