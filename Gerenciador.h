d3428e80ae4e23003cc86d055af1700f761184cf
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-17 12:22:49 -0300

    Update Gerenciador.h

--- Gerenciador.h
+++ Gerenciador.h
@@ -1,27 +1,30 @@
-#ifend GERENCIADOR_H
+#ifndef GERENCIADOR_H
 #define GERENCIADOR_H
 #include "Funcionario.h"
 #include "Operador.h"
 #include "Gerente.h"
 #include "Diretor.h"
 #include "Presidente.h"
+#include <vector>
 
-class Gerenciador{
+class Gerenciador {
   public:
   Gerenciador();
   ~Gerenciador();
   
-  int adicionarFuncionario();
-  int editarFuncionario();
-  int excluirFuncionario();
-  int exibirFuncionario();
-  int exibirLista();
-  int exibirListaTipo();
+  std::vector<Funcionario*> getVectorFuncionarios();
+  
+  void adicionarFuncionario();
+  void editarFuncionario();
+  void excluirFuncionario();
+  void exibirFuncionario();
+  void exibirLista();
+  void exibirListaTipo();
   
   protected:
   
   private:
-  
-}
+  std::vector<Funcionario*> vectorFuncionarios;
+};
 
 #endif
