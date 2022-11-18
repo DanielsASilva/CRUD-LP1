75aa63ea1e1c6ca1a179d1ace22ca2a7ccc8c826
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:52:43 -0300

    Update Gerente.cpp

--- Gerente.cpp
+++ Gerente.cpp
@@ -1,6 +1,8 @@
 #include "Gerente.h"
 #include <string>
     
+
+// Subclasse Gerente herda da superclasse Funcionario, então aqui só serão escritos os dados específicos
 Gerente::Gerente() : Funcionario(){
         
 }
@@ -22,89 +24,8 @@ Gerente::~Gerente(){
     
 }
 
-int Gerente::getCodigo(){
-     return codigo;   
-}
-
-       
-std::string Gerente::getNome(){
-    return nome;
-}
-    
-std::string Gerente::getEndereco(){
-    return endereco;
-}
-
-std::string Gerente::getTelefone(){
-    return telefone;
-}
-    
-int Gerente::getDataDia(){
-    return dataDia;
-}
-    
-int Gerente::getDataMes(){
-    return dataMes;
-}
-    
-int Gerente::getDataAno(){
-    return dataAno;
-}
-
-int Gerente::getTipoFuncionario(){
-    return tipoFuncionario;
-}
-
-std::string Gerente::getPosicao(){
-    return posicao;   
-}
-
-float Gerente::getSalario(){
-    return salario;
-}
-
 std::string Gerente::getAreaSupervisao(){
     return areaSupervisao;
-}
-  
-void Gerente::setCodigo(int code){
-    codigo = code;
-}
-
-void Gerente::setNome(std::string name){
-    nome = name;
-}
-
-void Gerente::setEndereco(std::string adress){
-    endereco = adress;
-}
-
-void Gerente::setTelefone(int phone){
-    telefone = phone;
-}
-
-void Gerente::setDataDia(int day){
-    dataDia = day;
-}
-
-void Gerente::setDataMes(int month){
-    dataMes = month;
-}
-
-void Gerente::setDataAno(int year){
-    dataAno = year;
-}
-
-void Gerente::setTipoFuncionario(int functionaryType){
-    tipoFuncionario = functionaryType;
-}
-
-void Gerente::setPosicao(std::string position){
-    posicao = position;   
-}
-
-void Gerente::setSalario(float salary){
-    salario = salary;
 }
 
 void Gerente::setAreaSupervisao(std::string supervisionArea){
