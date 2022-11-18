42d99e3366d312b59f9c50af64c8a272f86d2a26
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:36:52 -0300

    Update Gerente.h

--- Gerente.h
+++ Gerente.h
@@ -9,10 +9,9 @@ class Gerente : public Funcionario{
     Gerente(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string posicao, std::string telefone, float salario, std::string areaSupervisao);
     ~Gerente();
 
-    
     std::string getAreaSupervisao();
     void setAreaSupervisao(std::string supervisionArea);
-
+  
   private:
     std::string areaSupervisao;
 };
