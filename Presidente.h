1b1287e14151b3c659ef10ad2fa23d8cad73e6b5
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:29:40 -0300

    Update Presidente.h

--- Presidente.h
+++ Presidente.h
@@ -8,50 +8,16 @@ class Presidente : public Funcionario{
     Presidente();
     Presidente(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario, std::string areaFormacao, std::string areaAcademicaMax);
     ~Presidente();
-
-    int getCodigo();
-    std::string getNome();
-    std::string getEndereco();
-    std::string getTelefone();
-    int getDataDia();
-    int getDataMes();
-    int getDataAno();
-    int getTipoFuncionario();
-    std::string getPosicao();
-    float getSalario();
-    //std::string getAreaSupervisao();    
-    std::string getAreaAcademicaMax();
-    std::string getAreaFormacao();
   
-    void setCodigo(int code);
-    void setNome(std::string name);
-    void setEndereco(std::string adress);
-    void setTelefone(int phone);
-    void setDataDia(int day);
-    void setDataMes(int month);
-    void setDataAno(int year);
-    void setTipoFuncionario(int functionaryType);
-    void setPosicao(std::string position);
-    void setSalario(float salary);
-    //void setAreaSupervisao(std::string supervisionArea);    
+    std::string getAreaAcademicaMax();
+    std::string getAreaFormacao();   
     void setAreaAcademicaMax(std::string academiDegree);
     void setAreaFormacao(std::string academicWorkfield);
   
   protected:
-    int codigo;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    std::string posicao;
-    int dataDia;
-    int dataMes;
-    int dataAno;
-    int tipoFuncionario;
-    float salario;
-  
     std::string areaFormacao;
-  
     std::string areaAcademicaMax;
+  
   private:
 
 };
