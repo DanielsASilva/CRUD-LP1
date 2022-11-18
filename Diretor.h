1b0c08da0c6bf7a041332d918bfa7195bdc275d0
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:25:26 -0300

    Update Diretor.h

--- Diretor.h
+++ Diretor.h
@@ -10,50 +10,13 @@ class Diretor : public Funcionario{
     Diretor(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string posicao, std::string telefone, float salario, std::string areaSupervisao, std::string areaFormacao);
     ~Diretor();
 
-    int getCodigo();
-    std::string getNome();
-    std::string getEndereco();
-    std::string getTelefone();
-    int getDataDia();
-    int getDataMes();
-    int getDataAno();
-    int getTipoFuncionario();
-    std::string getPosicao();
-    float getSalario();
     std::string getAreaSupervisao();
     std::string getAreaFormacao();
-    //std::string getAreaAcademicaMax();
-  
-    void setCodigo(int code);
-    void setNome(std::string name);
-    void setEndereco(std::string adress);
-    void setTelefone(int phone);
-    void setDataDia(int day);
-    void setDataMes(int month);
-    void setDataAno(int year);
-    void setTipoFuncionario(int functionaryType);
-    void setPosicao(std::string position);
-    void setSalario(float salary);
     void setAreaSupervisao(std::string supervisionArea);
     void setAreaFormacao(std::string academicWorkfield);
-  
-    //void setAreaAcademicaMax(std::string academicDegree); 
-
-  protected:
 
   private:
-    /**int codigo;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    std::string posicao;
-    int dataDia;
-    int dataMes;
-    int dataAno;
-    int tipoFuncionario; **/
-  
     std::string areaSupervisao;
-    
     std::string areaFormacao;
 };
 
