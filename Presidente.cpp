b454c5edaced386d3e4dc024b1ee5127df3a1a20
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:58:21 -0300

    Update Presidente.cpp

--- Presidente.cpp
+++ Presidente.cpp
@@ -1,6 +1,7 @@
 #include "Presidente.h"
 #include <string>
     
+// Ao herdar as informações gerais de Funcionário, a classe Presidente adiciona apenas um dado novo, que é a formação acadêmica máxima 
 Presidente::Presidente() : Funcionario(){
         
 }
@@ -23,93 +24,12 @@ Presidente::~Presidente(){
     
 }
 
-int Presidente::getCodigo(){
-    return codigo;
-}
-
-       
-std::string Presidente::getNome(){
-    return nome;
-}
-    
-std::string Presidente::getEndereco(){
-    return endereco;
-}
-
-std::string Presidente::getTelefone(){
-    return telefone;
-}
-    
-int Presidente::getDataDia(){
-    return dataDia;
-}
-    
-int Presidente::getDataMes(){
-    return dataMes;
-}
-    
-int Presidente::getDataAno(){
-    return dataAno;
-}
-
-int Presidente::getTipoFuncionario(){
-    return tipoFuncionario;
-}
-
-std::string Presidente::getPosicao(){
-    return posicao;   
-}
-
-float Presidente::getSalario(){
-    return salario;
-}
-
 std::string Presidente::getAreaFormacao(){
     return areaFormacao;
 }
 
 std::string Presidente::getAreaAcademicaMax(){
     return areaAcademicaMax;
-}
-
-void Presidente::setCodigo(int code){
-    codigo = code;
-}
-
-void Presidente::setNome(std::string name){
-    nome = name;
-}
-
-void Presidente::setEndereco(std::string adress){
-    endereco = adress;
-}
-
-void Presidente::setTelefone(int phone){
-    telefone = phone;
-}
-
-void Presidente::setDataDia(int day){
-    dataDia = day;
-}
-
-void Presidente::setDataMes(int month){
-    dataMes = month;
-}
-
-void Presidente::setDataAno(int year){
-    dataAno = year;
-}
-
-void Presidente::setTipoFuncionario(int functionaryType){
-    tipoFuncionario = functionaryType;
-}
-    
-void Presidente::setPosicao(std::string position){
-    posicao = position;   
-}
-
-void Presidente::setSalario(float salary){
-    salario = salary;
 }
 
 void Presidente::setAreaFormacao(std::string academicWorkfield){
