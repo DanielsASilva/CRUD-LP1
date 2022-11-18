dfea7bf27713deaa9484441f4f407eaacf78dfa7
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 19:49:16 -0300

    Update Diretor.cpp

--- Diretor.cpp
+++ Diretor.cpp
@@ -20,94 +20,12 @@ Diretor::Diretor(int codigoFuncionario, std::string nome, std::string endereco, 
     this->areaFormacao = areaFormacao;
 }    
 
-
-int Diretor::getCodigo(){
-    return codigo;  
-}
-
-       
-std::string Diretor::getNome(){
-    return nome;
-}
-    
-std::string Diretor::getEndereco(){
-    return endereco;
-}
-
-std::string Diretor::getTelefone(){
-    return telefone;
-}
-    
-int Diretor::getDataDia(){
-    return dataDia;
-}
-    
-int Diretor::getDataMes(){
-    return dataMes;
-}
-    
-int Diretor::getDataAno(){
-    return dataAno;
-}
-
-int Diretor::getTipoFuncionario(){
-    return tipoFuncionario;
-}
-    
-std::string Diretor::getPosicao(){
-    return posicao;   
-}
-
-float Diretor::getSalario(){
-    return salario;
-}
-
 std::string Diretor::getAreaSupervisao(){
     return areaSupervisao;
 }
 
 std::string Diretor::getAreaFormacao(){
     return areaFormacao;
-}
-
-void Diretor::setCodigo(int code){
-    codigo = code;
-}
-
-void Diretor::setNome(std::string name){
-    nome = name;
-}
-
-void Diretor::setEndereco(std::string adress){
-    endereco = adress;
-}
-
-void Diretor::setTelefone(int phone){
-    telefone = phone;
-}
-
-void Diretor::setDataDia(int day){
-    dataDia = day;
-}
-
-void Diretor::setDataMes(int month){
-    dataMes = month;
-}
-
-void Diretor::setDataAno(int year){
-    dataAno = year;
-}
-
-void Diretor::setTipoFuncionario(int functionaryType){
-    tipoFuncionario = functionaryType;
-}
-    
-void Diretor::setPosicao(std::string position){
-    posicao = position;
-}
-
-void Diretor::setSalario(float salary){
-    salario = salary;
 }
 
 void Diretor::setAreaSupervisao(std::string supervisionArea){
