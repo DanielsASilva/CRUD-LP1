34329efded91bc7cad84adb94e62c602510d94e9
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-18 23:13:45 -0300

    Update Operador.h

--- Operador.h
+++ Operador.h
@@ -3,14 +3,12 @@
 #include <string>
 #include "Funcionario.h"
 
+// Não adicionando nenhum atributo que não esteja na superclasse, a classe Operador não instancia novos atributos
 class Operador : public Funcionario{
   public:
     Operador();
     Operador(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario);
     ~Operador();
-
-  private:
-    
 };
 
 #endif
