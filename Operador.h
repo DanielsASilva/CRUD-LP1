155ac1f314ef8d05f42399f172c45ba4f5ddd292
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:39:11 -0300

    Update Operador.h

--- Operador.h
+++ Operador.h
@@ -3,52 +3,12 @@
 #include <string>
 #include "Funcionario.h"
 
+// Não adicionando nenhum atributo que não esteja na superclasse, a classe Operador não instancia novos atributos
 class Operador : public Funcionario{
   public:
     Operador();
     Operador(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario);
     ~Operador();
-
-    int getCodigo();
-    std::string getNome();
-    std::string getEndereco();
-    std::string getTelefone();
-    int getDataDia();
-    int getDataMes();
-    int getDataAno();
-    int getTipoFuncionario();
-    std::string getPosicao();
-    float getSalario();
-    std::string getAreaSupervisao();
-    std::string getAreaFormacao();
-    std::string getAreaAcademicaMax();
-  
-    void setCodigo(int code);
-    void setNome(std::string name);
-    void setEndereco(std::string adress);
-    void setTelefone(int phone);
-    void setDataDia(int day);
-    void setDataMes(int month);
-    void setDataAno(int year);
-    void setPosicao(std::string position);
-    void setTipoFuncionario(int functionaryType);
-    void setSalario(float salary);
-    void setAreaSupervisao(std::string supervisionArea);
-    void setAreaFormacao(std::string academicWorkfield);
-    void setAreaAcademicaMax(std::string academicDegree); 
-  
-  protected:
-
-  private:
-    int codigo;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    std::string posicao;
-    int dataDia;
-    int dataMes;
-    int dataAno;
-    int tipoFuncionario;
 };
 
 #endif
