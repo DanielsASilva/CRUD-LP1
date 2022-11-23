4ec207477cbc3eade95e08aa88f401be7289c6f0
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:18:12 -0300

    Update Operador.cpp

--- Operador.cpp
+++ Operador.cpp
@@ -1,8 +1,9 @@
 #include "Operador.h"
 #include <string>
     
+// Como Operador herda todas as informações de Funcionário e não adiciona nenhum atributo novo, não há necessidade de repetir os gets e sets
 Operador::Operador() : Funcionario(){
-        
+    this->tipoFuncionario = 1;
 }
 
 Operador::Operador(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone ,std::string posicao, float salario){
@@ -15,89 +16,9 @@ Operador::Operador(int codigoFuncionario, std::string nome, std::string endereco
     this->telefone = telefone;
     this->posicao = posicao;
     this->salario = salario;
+    this->tipoFuncionario = 1;
 }
 
 Operador::~Operador(){
     
-}
-
-int Operador::getCodigo(){
-    return codigo;
-}
-
-       
-std::string Operador::getNome(){
-    return nome;
-}
-    
-std::string Operador::getEndereco(){
-    return endereco;
-}
-
-std::string Operador::getTelefone(){
-    return telefone;
-}
-    
-int Operador::getDataDia(){
-    return dataDia;
-}
-    
-int Operador::getDataMes(){
-    return dataMes;
-}
-    
-int Operador::getDataAno(){
-    return dataAno;
-}
-
-int Operador::getTipoFuncionario(){
-    return tipoFuncionario;
-}
-    
-std::string Operador::getPosicao(){
-    return posicao;   
-}
-
-float Operador::getSalario(){
-    return salario;
-}
-  
-void Operador::setCodigo(int code){
-    codigo = code;
-}
-
-void Operador::setNome(std::string name){
-    nome = name;
-}
-
-void Operador::setEndereco(std::string adress){
-    endereco = adress;
-}
-
-void Operador::setTelefone(int phone){
-    telefone = phone;
-}
-
-void Operador::setDataDia(int day){
-    dataDia = day;
-}
-
-void Operador::setDataMes(int month){
-    dataMes = month;
-}
-
-void Operador::setDataAno(int year){
-    dataAno = year;
-}
-
-void Operador::setTipoFuncionario(int functionaryType){
-    tipoFuncionario = functionaryType;
-}
-    
-void Operador::setPosicao(std::string position){
-    posicao = position;   
-}
-
-void Operador::setSalario(float salary){
-    salario = salary;
 }
