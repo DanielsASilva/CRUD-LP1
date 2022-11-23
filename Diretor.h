72cceb7b3947c76134fc3cd9e23a877c9818a11e
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:13:43 -0300

    Update Diretor.h

--- Diretor.h
+++ Diretor.h
@@ -1,60 +1,24 @@
-#ifndef DIREITO_H //não era pra ser DIRETOR_H?
-#define DIREITO_H
+#ifndef DIRETOR_H
+#define DIRETOR_H
 #include <string>
 #include "Gerente.h"
 
 //.h padrão com "get"s e "set"s. Na classe diretor, os dados Area de Supervisao e Area de Formacao estão inclusos
-class Diretor : public Gerente{
+class Diretor : public Funcionario{
   public:
     Diretor();
     Diretor(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string posicao, std::string telefone, float salario, std::string areaSupervisao, std::string areaFormacao);
     ~Diretor();
 
-    int getCodigo();
-    std::string getNome();
-    std::string getEndereco();
-    std::string getTelefone();
-    int getDataDia();
-    int getDataMes();
-    int getDataAno();
-    int getTipoFuncionario();
-    std::string getPosicao();
-    float getSalario();
     std::string getAreaSupervisao();
     std::string getAreaFormacao();
-    std::string getAreaAcademicaMax();
-  
-    void setCodigo(int code);
-    void setNome(std::string name);
-    void setEndereco(std::string adress);
-    void setTelefone(int phone);
-    void setDataDia(int day);
-    void setDataMes(int month);
-    void setDataAno(int year);
-    void setTipoFuncionario(int functionaryType);
-    void setPosicao(std::string position);
-    void setSalario(float salary);
     void setAreaSupervisao(std::string supervisionArea);
     void setAreaFormacao(std::string academicWorkfield);
   
-    //void setAreaAcademicaMax(std::string academicDegree); 
-
-  protected:
-
   private:
-    int codigo;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    std::string posicao;
-    int dataDia;
-    int dataMes;
-    int dataAno;
-    int tipoFuncionario;
-  
     std::string areaSupervisao;
-    
     std::string areaFormacao;
+
 };
 
 #endif
