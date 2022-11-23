62c47880aa5e62b26a4f6c04bd8797a5fc69a44c
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:14:17 -0300

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -1,9 +1,97 @@
 #include "Funcionario.h"
 
+// Conjunto de gets e sets da superclasse Funcionario. O construtor "Funcionario()" irá inicializar os valores dos atributos
+//que não são strings com zero.
 Funcionario::Funcionario(){
- 
+    setCodigo(0);
+    setDataDia(0);
+    setDataMes(0);
+    setDataAno(0);
+    setSalario(0);
+    setTipoFuncionario(0);
 }
 
+// Não é necessário implementar o destrutor porque a remoção de um funcionário ocorrerá no arquivo
 Funcionario::~Funcionario(){
 
+}
+
+int Funcionario::getCodigo(){
+    return codigo;
+}
+
+std::string Funcionario::getNome(){
+    return nome;
+}
+    
+std::string Funcionario::getEndereco(){
+    return endereco;
+}
+
+std::string Funcionario::getTelefone(){
+    return telefone;
+}
+    
+int Funcionario::getDataDia(){
+    return dataDia;
+}
+    
+int Funcionario::getDataMes(){
+    return dataMes;
+}
+    
+int Funcionario::getDataAno(){
+    return dataAno;
+}
+
+int Funcionario::getTipoFuncionario(){
+    return tipoFuncionario;
+}
+
+std::string Funcionario::getPosicao(){
+    return posicao;   
+}
+
+float Funcionario::getSalario(){
+    return salario;
+}
+
+void Funcionario::setCodigo(int code){
+    codigo = code;
+}
+
+void Funcionario::setNome(std::string name){
+    nome = name;
+}
+
+void Funcionario::setEndereco(std::string adress){
+    endereco = adress;
+}
+
+void Funcionario::setTelefone(std::string phone){
+    telefone = phone;
+}
+
+void Funcionario::setDataDia(int day){
+    dataDia = day;
+}
+
+void Funcionario::setDataMes(int month){
+    dataMes = month;
+}
+
+void Funcionario::setDataAno(int year){
+    dataAno = year;
+}
+
+void Funcionario::setTipoFuncionario(int functionaryType){
+    tipoFuncionario = functionaryType;
+}
+    
+void Funcionario::setPosicao(std::string position){
+    posicao = position;   
+}
+
+void Funcionario::setSalario(float salary){
+    salario = salary;
 }
