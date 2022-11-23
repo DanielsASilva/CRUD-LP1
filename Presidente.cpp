4bbc9f9e48c962d0c88f8a5888d9408c541c64e8
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:39:46 -0300

    Update Presidente.cpp

--- Presidente.cpp
+++ Presidente.cpp
@@ -1,8 +1,9 @@
 #include "Presidente.h"
 #include <string>
     
+// Ao herdar as informações gerais de Funcionário, a classe Presidente adiciona apenas um dado novo, que é a formação acadêmica máxima 
 Presidente::Presidente() : Funcionario(){
-        
+    this->tipoFuncionario = 4;
 }
 
 Presidente::Presidente(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario, std::string areaFormacao, std::string areaAcademicaMax){
@@ -15,6 +16,7 @@ Presidente::Presidente(int codigoFuncionario, std::string nome, std::string ende
     this->posicao = posicao;
     this->telefone = telefone;
     this->salario = salario;
+    this->tipoFuncionario = 4;
     this->areaFormacao = areaFormacao;
     this->areaAcademicaMax = areaAcademicaMax;
 }
@@ -23,93 +25,12 @@ Presidente::~Presidente(){
     
 }
 
-int Presidente::getCodigo(){
-    return codigo;
-}
-
-       
-std::string Presidente::getNome(){
-    return nome;
-}
-    
-std::string Presidente::getEndereco(){
-    return endereco;
-}
-
-std::string Presidente::getTelefone(){
-    return telefone;
-}
-    
-int Presidente::getDataDia(){
-    return dataDia;
-}
-    
-int Presidente::getDataMes(){
-    return dataMes;
-}
-    
-int Presidente::getDataAno(){
-    return dataAno;
-}
-
-int Presidente::getTipoFuncionario(){
-    return tipoFuncionario;
-}
-
-std::string Presidente::getPosicao(){
-    return posicao;   
-}
-
-float Presidente::getSalario(){
-    return salario;
-}
-
 std::string Presidente::getAreaFormacao(){
     return areaFormacao;
 }
 
 std::string Presidente::getAreaAcademicaMax(){
     return areaAcademicaMax;
-}
-
-void Presidente::setCodigo(int code){
-    codigo = code;
-}
-
-void Presidente::setNome(std::string name){
-    nome = name;
-}
-
-void Presidente::setEndereco(std::string adress){
-    endereco = adress;
-}
-
-void Presidente::setTelefone(int phone){
-    telefone = phone;
-}
-
-void Presidente::setDataDia(int day){
-    dataDia = day;
-}
-
-void Presidente::setDataMes(int month){
-    dataMes = month;
-}
-
-void Presidente::setDataAno(int year){
-    dataAno = year;
-}
-
-void Presidente::setTipoFuncionario(int functionaryType){
-    tipoFuncionario = functionaryType;
-}
-    
-void Presidente::setPosicao(std::string position){
-    posicao = position;   
-}
-
-void Presidente::setSalario(float salary){
-    salario = salary;
 }
 
 void Presidente::setAreaFormacao(std::string academicWorkfield){
