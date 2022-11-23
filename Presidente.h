25a32a9c0182e29572992f48d26386a0e68c159a
AndreLTeles <118027222+AndreLTeles@users.noreply.github.com>
2022-11-23 12:41:17 -0300

    Update Presidente.h

--- Presidente.h
+++ Presidente.h
@@ -3,57 +3,21 @@
 #include <string>
 #include "Funcionario.h"
 
+// Na classe Presidente, os atributos da Area de Formação e da Formação Acadêmica, exclusivos desta classe, são instanciados 
 class Presidente : public Funcionario{
   public:
     Presidente();
     Presidente(int codigoFuncionario, std::string nome, std::string endereco, int dia, int mes, int ano, std::string telefone, std::string posicao, float salario, std::string areaFormacao, std::string areaAcademicaMax);
     ~Presidente();
-
-    int getCodigo();
-    std::string getNome();
-    std::string getEndereco();
-    std::string getTelefone();
-    int getDataDia();
-    int getDataMes();
-    int getDataAno();
-    int getTipoFuncionario();
-    std::string getPosicao();
-    float getSalario();
-    std::string getAreaSupervisao();    
-    std::string getAreaAcademicaMax();
-    std::string getAreaFormacao();
   
-    void setCodigo(int code);
-    void setNome(std::string name);
-    void setEndereco(std::string adress);
-    void setTelefone(int phone);
-    void setDataDia(int day);
-    void setDataMes(int month);
-    void setDataAno(int year);
-    void setTipoFuncionario(int functionaryType);
-    void setPosicao(std::string position);
-    void setSalario(float salary);
-    void setAreaSupervisao(std::string supervisionArea);    
+    std::string getAreaAcademicaMax();
+    std::string getAreaFormacao();   
     void setAreaAcademicaMax(std::string academiDegree);
     void setAreaFormacao(std::string academicWorkfield);
   
-  protected:
-    int codigo;
-    std::string nome;
-    std::string endereco;
-    std::string telefone;
-    std::string posicao;
-    int dataDia;
-    int dataMes;
-    int dataAno;
-    int tipoFuncionario;
-    float salario;
-  
-    std::string areaFormacao;
-  
-    std::string areaAcademicaMax;
   private:
-
+  std::string areaFormacao;
+  std::string areaAcademicaMax;
 };
 
 #endif
